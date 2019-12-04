8a7b7656556128326b6dd4f6e366a41f3cf4ed96
luizomatias <luizomatias1@yahoo.com.br>
2019-12-04 13:53:49 -0300

    'final_commit'

--- src/components/main.js
+++ src/components/main.js
@@ -9,6 +9,7 @@ import Projects from './projects';
 const Main = () => (
   <Switch>
     <Route exact path="/" component={LandingPage}/>
+    <Route exact path="/portfolio" component={LandingPage}/>
     <Route  path="/aboutme" component={About}/>
     <Route  path="/contact" component={Contact}/>
     <Route  path="/projects" component={Projects}/>
