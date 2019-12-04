128ae962cb36e6eb82619b6e6c3ad1953ade015a
luizomatias <luizomatias1@yahoo.com.br>
2019-12-04 13:15:41 -0300

    Updates

---
+++ precache-manifest.0480bce02d8182a6474da1bd432cd98b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119068538573e49cfc398baa52db9b15",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e160b94456577ad60875",
+    "url": "/portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "09e808b06841e632bf8c",
+    "url": "/portfolio/static/css/main.f219d396.chunk.css"
+  },
+  {
+    "revision": "e160b94456577ad60875",
+    "url": "/portfolio/static/js/2.8e7b2981.chunk.js"
+  },
+  {
+    "revision": "09e808b06841e632bf8c",
+    "url": "/portfolio/static/js/main.f7d456e4.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5b6eb78f222b7077ea5891d864909184",
+    "url": "/portfolio/static/media/ds.5b6eb78f.png"
+  },
+  {
+    "revision": "198d2da31f86559ad10be288a134a932",
+    "url": "/portfolio/static/media/perfil.198d2da3.png"
+  },
+  {
+    "revision": "5ed18c32ab45966a3f2a17286ac48c0d",
+    "url": "/portfolio/static/media/perfil2.5ed18c32.png"
+  },
+  {
+    "revision": "006ba18e8b24272bbb5989d08e29afa2",
+    "url": "/portfolio/static/media/projetoia.006ba18e.png"
+  },
+  {
+    "revision": "3b47c51fce2a36dc7d8b27fcda788bcd",
+    "url": "/portfolio/static/media/robo.3b47c51f.png"
+  }
+]);
